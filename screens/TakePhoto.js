771ce2fce399651632f1b3797064843f5abc6ecd
Michael D <michael@codetropolis.com>
2021-10-13 09:18:30 -0500

    TakePhoto Page - WIP.

--- screens/TakePhoto.js
+++ screens/TakePhoto.js
@@ -1,12 +1,19 @@
-import React, { useLayoutEffect, useState, useEffect } from 'react'
-import { StyleSheet, Text, View, TouchableOpacity, Alert } from 'react-native'
+import React, { useState, useEffect } from 'react'
+import { StyleSheet, Text, View, TouchableOpacity, Alert, ImageBackground } from 'react-native'
 import { StatusBar } from 'expo-status-bar'
 import { Camera } from 'expo-camera';
-
+import CameraPreview from '../components/CameraPreview'
+let camera
 
 const TakePhoto = () => {
 
     const [startCamera, setStartCamera] = useState(false)
+    const [previewVisible, setPreviewVisible] = useState(false)
+    const [capturedImage, setCapturedImage] = useState(null)
+    const [cameraType, setCameraType] = useState(Camera.Constants.Type.back)
+    const [flashMode, setFlashMode] = useState('off')
+
+
 
     const __startCamera = async () => {
         const { status } = await Camera.requestPermissionsAsync()
@@ -18,16 +25,145 @@ const TakePhoto = () => {
         }
     }
 
+    const __takePicture = async () => {
+        if (!camera) return
+        const photo = await camera.takePictureAsync()
+        console.log(photo)
+        setPreviewVisible(true)
+        setCapturedImage(photo)
+    }
+    const __savePhoto = () => {
+        console.log(`@CodeTropolis ~ TakePhoto ~ __savePhoto capturedImage`, capturedImage);
+        setPreviewVisible(false)
+    }
+    const __retakePicture = () => {
+        setCapturedImage(null)
+        setPreviewVisible(false)
+        __startCamera()
+    }
+    const __handleFlashMode = () => {
+        if (flashMode === 'on') {
+            setFlashMode('off')
+        } else if (flashMode === 'off') {
+            setFlashMode('on')
+        } else {
+            setFlashMode('auto')
+        }
+    }
+    const __switchCamera = () => {
+        if (cameraType === 'back') {
+            setCameraType('front')
+        } else {
+            setCameraType('back')
+        }
+    }
+
     return (
         <View style={styles.container}>
-
             {startCamera ? (
-                <Camera
-                    style={{ flex: 1, width: "100%" }}
-                    ref={(r) => {
-                        camera = r
+                <View
+                    style={{
+                        flex: 1,
+                        width: '100%'
                     }}
-                ></Camera>
+                >
+                    {previewVisible && capturedImage ? (
+                        <CameraPreview photo={capturedImage} savePhoto={__savePhoto} retakePicture={__retakePicture} />
+                    ) : (
+                        <Camera
+                            type={cameraType}
+                            flashMode={flashMode}
+                            style={{ flex: 1 }}
+                            ref={(r) => {
+                                camera = r
+                            }}
+                        >
+                            <View
+                                style={{
+                                    flex: 1,
+                                    width: '100%',
+                                    backgroundColor: 'transparent',
+                                    flexDirection: 'row'
+                                }}
+                            >
+                                <View
+                                    style={{
+                                        position: 'absolute',
+                                        left: '5%',
+                                        top: '10%',
+                                        flexDirection: 'column',
+                                        justifyContent: 'space-between'
+                                    }}
+                                >
+                                    <TouchableOpacity
+                                        onPress={__handleFlashMode}
+                                        style={{
+                                            backgroundColor: flashMode === 'off' ? '#000' : '#fff',
+                                            borderRadius: 50,
+                                            height: 25,
+                                            width: 25
+                                        }}
+                                    >
+                                        <Text
+                                            style={{
+                                                fontSize: 20
+                                            }}
+                                        >
+                                            ⚡️
+                                        </Text>
+                                    </TouchableOpacity>
+                                    <TouchableOpacity
+                                        onPress={__switchCamera}
+                                        style={{
+                                            marginTop: 20,
+                                            borderRadius: 50,
+                                            height: 25,
+                                            width: 25
+                                        }}
+                                    >
+                                        <Text
+                                            style={{
+                                                fontSize: 20,
+                                            }}
+                                        >
+                                            {cameraType === 'front' ? '🤳' : '📷'}
+                                        </Text>
+                                    </TouchableOpacity>
+                                </View>
+                                <View
+                                    style={{
+                                        position: 'absolute',
+                                        bottom: 0,
+                                        flexDirection: 'row',
+                                        flex: 1,
+                                        width: '100%',
+                                        padding: 20,
+                                        justifyContent: 'space-between'
+                                    }}
+                                >
+                                    <View
+                                        style={{
+                                            alignSelf: 'center',
+                                            flex: 1,
+                                            alignItems: 'center'
+                                        }}
+                                    >
+                                        <TouchableOpacity
+                                            onPress={__takePicture}
+                                            style={{
+                                                width: 70,
+                                                height: 70,
+                                                bottom: 0,
+                                                borderRadius: 50,
+                                                backgroundColor: '#fff'
+                                            }}
+                                        />
+                                    </View>
+                                </View>
+                            </View>
+                        </Camera>
+                    )}
+                </View>
             ) : (
                 <View
                     style={{
@@ -56,11 +192,13 @@ const TakePhoto = () => {
                                 textAlign: 'center'
                             }}
                         >
-                            Take picture
+                            Take Photo
                         </Text>
                     </TouchableOpacity>
                 </View>
             )}
+
+            <StatusBar style="auto" />
         </View>
     )
 }
@@ -75,3 +213,5 @@ const styles = StyleSheet.create({
         justifyContent: 'center'
     }
 })
+
+
